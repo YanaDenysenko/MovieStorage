aeedbad3d7afeaf034e8ca4835ea6863ec4c0b2d
Yana <yana.denysenko52@gmail.com>
2022-07-03 22:01:37 +0300

    feat: New updates.

--- src/main/java/org/example/repository/impl/UserRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/UserRepositoryImpl.java
@@ -2,10 +2,16 @@ package org.example.repository.impl;
 
 import org.example.model.User;
 import org.example.repository.UserRepository;
+import org.example.utils.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
 
 public class UserRepositoryImpl implements UserRepository {
     @Override
     public User getById(long userId) {
-        return null;
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, userId);
+    }
+
+    public void saveUser(User user){
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
     }
 }
